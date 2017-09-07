dd01149624f057d6d5500ad04c827b53ffa2ec81
gabrielash <gash234@yahoo.com>
2017-09-07 06:04:42 +0200

    MsgRecv added

--- README.md
+++ README.md
@@ -213,7 +213,22 @@ These are the main classes providing a higher-level Perl6 OO interface to ZMQ
         bytes( --> Int)          
         segments( --> Int)  
         copy( --> Str)
-        
+
+#####  MsgRecv
+    MsgRecv accumulates message parts received on one or more sockets with minimal copying.
+    parts can be examined, slectevely sent over sockets. and transforming functions can be
+    queued on each part.
+
+    methods:
+        slurp(Socket, :async)
+                  accumulate waiting message parts from a socket
+        push-transform(UInt, &func)
+                  queue a transfrm function on a message part. The function should
+                  conform to :(Str:D --> Str:D|Any ). Any effectively delete the part.         
+        send(Socket, $from = 0, $n = elems, :async ) sends all or ome of the parts
+        [ UInt ] returns message part as Str
+
+
 
 #####   PollBuilder
     PollBuilder builds a polled set of receiving sockets
--- lib/Net/ZMQ/Message.pm
+++ lib/Net/ZMQ/Message.pm
@@ -11,15 +11,19 @@ use Net::ZMQ::Error;
 use Net::ZMQ::Common;
 use Net::ZMQ::Socket;
 
-class MsgIterator {...}
 class MsgBuilder {...}
 class MsgRecv-impl {...}
 
 class MsgRecv is export does Positional does Iterable {
   my $doc := q:to/END/;
 
-  END
-  #:
+   methods
+   push-transform(UInt, &func)
+   slurp(Socket, -async)
+   send(Socket, $from = 0, $n =  self.elems, :async )
+
+   END
+   #:
 
   has MsgRecv-impl $!mimpl handles < elems keys > .= new;
   has %!transformers;
@@ -75,8 +79,8 @@ class MsgRecv is export does Positional does Iterable {
   }
 
   method send(Socket $socket, Int $from where ^self.elems = 0
-                                      , Int $n where positive($n) =  self.elems
-                                      , :$async ) {
+                            , Int $n where positive($n) =  self.elems
+                            , :$async ) {
 
     $from = 0 unless $from.defined;
     my $last = $from + $n - 1;
@@ -139,16 +143,13 @@ class Buffer {
       iterator( --> MsgIterator)  - return a segment Iterator
       offset-pointer(Int i --> Pointer) - returns a Pointer to the buffer's byte i location in memory
 
-  END
-  #:
+    END
+    #:
 
-  has buf8 $.buffer     is rw = buf8.new;
-  has uint @.offsets    is rw;
+  has buf8 $.buffer  is rw = buf8.new;
+  has uint @.offsets handles  < AT-POS  >;
   has Int  $.next-i     is rw = 0;
 
-  method iterator( --> MsgIterator) {
-    return MsgIterator.new(self);
-  }
 
   method bytes( --> Int ) { return $!next-i; }
 
@@ -164,53 +165,6 @@ class Buffer {
 
   method copy( --> Str ) {
      return $!buffer.decode('ISO-8859-1');
-  }
-}
-
-class MsgIterator {
-  my $doc := q:to/END/;
-
-    Forward Iterator over the Message class, returns a series of segments sizes in
-    bytes. example
-
-      my $it = $buffer.iterator;
-      my $from = 0;
-      while $it->has-next {
-        $next = $it.next;
-        say "segment is from offset $from to { $next - 1 }";
-        $from = $next;
-      }
-
-  END
-  #:
-
-  has Buffer $!buffer handles < segments bytes >;
-  has Int $!i;
-  has Int $!offset;
-
-
-  method TWEAK {
-    die "MsgIterator needs an instance" unless $!buffer.defined;
-    $!i = 0;
-    $!offset = Int;
-  }
-
-  submethod BUILD( :$buffer ) { $!buffer := $buffer; }
-
-  method new(Buffer $buffer) {
-    return self.bless( :$buffer );
-  }
-
-  method next( --> Int  ) {
-      die "illegal offset" if $!offset > self.bytes;
-      return $!offset;
-  }
-
-  method has-next( --> Bool)  {
-    return False if $!i == self.segments;#$!segments;
-    $!offset = $!buffer.offset( $!i++ );
-    die "asserting offset not in overflow" unless ($!offset <= self.bytes);
-    return True;
   }
 }
 
@@ -232,8 +186,8 @@ class Message is export  {
         bytes()
         segments()
 
-  END
-  #:
+    END
+    #:
 
   has Str $.encoding;   # not implemented yet
 
@@ -251,20 +205,20 @@ class Message is export  {
   method send(Socket:D $socket, :$part, :$async, :$callback where sub( $callback )
                           , :$verbose ) {
     my $doc := q:to/END/;
-    sends the assembled message in segments with zero-copy
-    part - sets the last part as incopmlete
-    callback - specifies a callback function for ZMQ
-    async - duh!
+      sends the assembled message in segments with zero-copy
+      part - sets the last part as incopmlete
+      callback - specifies a callback function for ZMQ
+      async - duh!
 
-    Uses Nativcast Pointer arithmatic to avoid copying of data in order to benefit from
-    the optimizations of ZMQ zero-copy. Offsets into the buffer are sent as
-    arguments to ZMQ with the assumption that the buffer is an immutable byte
-    array in continguous memory. Caveat Emptor!
+      Uses Nativcast Pointer arithmatic to avoid copying of data in order to benefit from
+      the optimizations of ZMQ zero-copy. Offsets into the buffer are sent as
+      arguments to ZMQ with the assumption that the buffer is an immutable byte
+      array in continguous memory. Caveat Emptor!
 
-    The default callback has to be threadsafe, and it is not, yet!   #ISSUE
+      The default callback has to be threadsafe, and it is not, yet!   #ISSUE
 
-    END
-    #:
+      END
+      #:
 
         # INVESTIGATE: not sure how zmq_msg_init_data behaves without a callback
         # does it takes ownership and free the memory? Then callbacks are mandatory
@@ -279,13 +233,12 @@ class Message is export  {
 
     my $sent = 0;
     my $segments = self.segments;
-    my MsgIterator  $it = $!_.iterator;
     my $i = 0;
 
-    while $it.has-next {
-      my $end = $it.next;
+    for $!_.offsets  -> $end {
       my zmq_msg_t $msg-t .= new;
-      my $ptr = ($end > $i) ?? $!_.offset-pointer($i)
+
+      my Pointer $ptr = ($end > $i) ?? $!_.offset-pointer($i)
                             !! Pointer;
       my $r = $callback.defined && $callback.WHAT === Sub
                     ?? zmq_msg_init_data_callback($msg-t,$ptr , $end - $i, $callback)
@@ -304,8 +257,8 @@ class Message is export  {
 
 class MsgBuilder is export {
   my $doc= q:to/END/;
-  Class MsgBuilder builds a Message Object that can be used to send complex message
-  using zero-copy.
+    Class MsgBuilder builds a Message Object that can be used to send complex message
+    using zero-copy.
 
       USAGE example
         my MsgBuilder $builder  .= new;
@@ -319,17 +272,17 @@ class MsgBuilder is export {
 
 
 
-  Methods
+      Methods
       new()
       add( Str, -max-part-size -divide-into, -newline --> self)
       add( -empty --> self)
       add( -newline --> self)
       finalize( --> Message)
 
-  ATTN - replace - (dash) with colon-dollar in signatures above
+    ATTN - replace - (dash) with colon-dollar in signatures above
             (subtitution is to please Atom syntax-highlighter)
-  END
-  #:
+    END
+    #:
 
   has Str $.encoding;   # not implemented yet
 
